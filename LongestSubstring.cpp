a00f9faf5127527efb398a0385a2ac9089e09414
Nevin Abraham <160344891+vers3s@users.noreply.github.com>
2025-02-17 23:13:26 +0530

    Add files via upload

---
+++ LongestSubstring.cpp
@@ -0,0 +1,22 @@
+#include <unordered_set>
+#include <string>
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(std::string s) {
+        std::unordered_set<char> charSet;
+        int left = 0, maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            while (charSet.find(s[right]) != charSet.end()) {
+                charSet.erase(s[left]);
+                left++;
+            }
+            charSet.insert(s[right]);
+            maxLength = std::max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+};
+
