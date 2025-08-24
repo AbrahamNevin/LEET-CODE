281c9d81c64b11ae392c27386b922da60958e184
Nevin Abraham <160344891+AbrahamNevin@users.noreply.github.com>
2025-08-24 23:07:32 +0530

    Create groupAnagrams.java

---
+++ groupAnagrams.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) return new ArrayList<>();
+        
+        Map<String, List<String>> map = new HashMap<>();
+        
+        for (String s : strs) {
+            char[] charArr = s.toCharArray();
+            Arrays.sort(charArr);
+            String key = new String(charArr);
+            
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(s);
+        }
+        
+        return new ArrayList<>(map.values());
+    }
+}
